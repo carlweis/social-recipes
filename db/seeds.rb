020b0d508e018155ec595f3378d105c8cd1b3c1a
Carl Weis <carl@carlweis.com>
2016-06-22 22:56:47 -0500

    Setup new recipe form

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Food Types
+food_types = ["Pasta", "Dessert", "Sides", "Lunch", "Breakfast"]
+food_types.each{|d| FoodType.where(:name => d).first_or_create}
+
+# Food Preferences
+food_preferences = ["Vegetarian", "Vegan", "Meat", "Dairy"]
+food_preferences.each{|d| FoodPreference.where(:name => d).first_or_create}
+
+# Cuisines
+cuisines = ["Italian", "Mexican", "Indian", "Chinese"]
+cuisines.each{|d| Cuisine.where(:name => d).first_or_create}
